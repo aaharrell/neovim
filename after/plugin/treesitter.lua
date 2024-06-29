e27a1ddd6e35b7fbfa6d1519b2dcf6eedce08c96
Austen Harrell <austen.harrell@ulteig.com>
2024-06-28 18:41:27 -0600

    Modified init.lua file with '<leader>wd' functionality to write neovim explorer pwd

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -1,6 +1,6 @@
 require'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all" (the five listed parsers should always be installed)
-  ensure_installed = { "bash", "c", "cpp", "c_sharp", "python", "rust", "vim", "vimdoc", "query" },
+  ensure_installed = { "bash", "c", "cpp", "c_sharp", "go", "python", "rust", "vim", "vimdoc", "query" },
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
